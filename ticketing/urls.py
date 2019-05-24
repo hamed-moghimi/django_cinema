1db4151bfde7177048a1f1ce854787641e236049
Hamed Moghimi <shmoghimi70@gmail.com>
2019-05-25 01:59:47 +0430

    movie & cinema list templates added (html only) urls naming and links added

--- ticketing/urls.py
+++ ticketing/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'ticketing'
 urlpatterns = [
-    path('movie/list/', views.movie_list),
-    path('cinema/list/', views.cinema_list),
+    path('movie/list/', views.movie_list, name='movie_list'),
+    path('cinema/list/', views.cinema_list, name='cinema_list'),
 ]
--- ticketing/views.py
+++ ticketing/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 from .models import Movie, Cinema
@@ -6,27 +5,15 @@ from .models import Movie, Cinema
 
 def movie_list(request):
     movies = Movie.objects.all()
-    response_text = '\n'.join(
-        '{}: {}'.format(i, movie) for i, movie in enumerate(movies, start=1)
-    )
-    return HttpResponse(response_text)
+    context = {
+        'movies': movies
+    }
+    return render(request, 'ticketing/movie_list.html', context)
 
 
 def cinema_list(request):
     cinemas = Cinema.objects.all()
-    response_text = """
-    <html>
-    <head>
-    <title>لیست سینماها</title>
-    </head>
-    <body>
-        <h1>فهرست سینماهای کشور</h1>
-        <ul>
-            {}
-        </ul>
-    </body>
-    </html>
-    """.format(
-        '\n'.join('<li>{}</li>'.format(cinema) for cinema in cinemas)
-    )
-    return HttpResponse(response_text)
+    context = {
+        'cinemas': cinemas
+    }
+    return render(request, 'ticketing/cinema_list.html', context)

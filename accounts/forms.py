2e2891b703e46f355b932ee4a02c401fe1d48f5d
Hamed Moghimi <shmoghimi70@gmail.com>
2019-12-13 12:08:35 +0330

    payment form clean methods

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from accounts.models import Payment
 
@@ -8,3 +9,29 @@ class PaymentForm(forms.ModelForm):
         model = Payment
         fields = ['amount', 'transaction_code']
         # or: exclude = ['profile', 'transaction_time']
+
+    def clean_transaction_code(self):
+        code = self.cleaned_data.get('transaction_code')
+        try:
+            # should be in format: bank-<amount>-<TOKEN>#
+            # e.g. bank-30000-UHB454GRH73BDYU#
+            assert code.startswith('bank-')
+            assert code.endswith('#')
+            int(code.split('-')[1])
+        except:
+            raise ValidationError('قالب رسید تراکنش معتبر نیست')
+        return code
+
+    def clean_amount(self):
+        amount = self.cleaned_data.get('amount')
+        if amount % 1000 != 0:
+            raise ValidationError('مبلغ پرداختی باید ضریبی از هزار تومان باشد')
+        return amount
+
+    def clean(self):
+        super().clean()
+        code = self.cleaned_data.get('transaction_code')
+        amount = self.cleaned_data.get('amount')
+        if code and amount:
+            if int(code.split('-')[1]) != amount:
+                raise ValidationError('رسید و مبلغ تراکنش هم‌خوانی ندارند')
